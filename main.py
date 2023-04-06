459c1441c0a38e857b2e3fc971c43ecddf882031
Ash <ashbiju17@gmail.com>
2023-04-06 12:37:12 +0530

    frontend

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

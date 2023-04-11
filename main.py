8d4f83ca99a382feface801c4bd11201bcf8e34f
Ash <ashbiju17@gmail.com>
2023-04-11 22:26:22 +0530

    re arranged

--- main.py
+++ main.py
@@ -4,19 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/aboutus')
-def aboutus():
-    return render_template('aboutus.html')
-
-@app.route('/services')
-def services():
-    return render_template('services.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

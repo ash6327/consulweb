415d510341a0b51b7ad8731cad40f64ec0a04588
Ash <ashbiju17@gmail.com>
2023-04-12 15:33:54 +0530

    day 1

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import os
+import sqlite3
+import hashlib
+import time
 from flask import Flask, request, render_template,url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/prebuilt')
+def prebuilt():
+    return render_template('prebuilt.html')
+
+@app.route('/3dprint')
+def print():
+    return render_template('3dprinting.html')
 
 
 if __name__ == '__main__':
